ebe71228124ebaf92699a73743db798635687de1
Marie Thompson <mariethompson35@gmail.com>
2021-07-17 13:40:41 -0700

    create CitySearch test

--- src/_test_/App.test.js
+++ src/_test_/App.test.js
@@ -9,4 +9,9 @@ describe('<App /> component', () => {
     const AppWrapper = shallow(<App />);
     expect(AppWrapper.find(EventList)).toHaveLength(1);
   });
+
+  test('render CitySearch', () => {
+    const AppWrapper = shallow(<App />);
+    expect(AppWrapper.find(CitySearch)).toHaveLength(1);
+  });
 });
