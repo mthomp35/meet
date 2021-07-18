71d86cf47bdd58bbf31738a363debf2efb882b5e
Marie Thompson <mariethompson35@gmail.com>
2021-07-18 13:54:05 -0700

    Create CitySearch js & test

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -4,6 +4,13 @@ class CitySearch extends Component {
   render() {
     return (
       <div className='CitySearch'>
+        <input
+          type='text'
+          className='city'
+        />
+        <ul className='suggestions'>
+          
+        </ul>
       </div>
     );
   }
