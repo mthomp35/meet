8f58e43822744e2c78a0cc378fc632644e51d0e8
Marie Thompson <mariethompson35@gmail.com>
2021-07-18 14:23:11 -0700

    Create state and test for changing state

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 
 class CitySearch extends Component {
+  state = {
+    query: ''
+  }
+
   render() {
     return (
       <div className='CitySearch'>
         <input
           type='text'
           className='city'
+          value={this.state.query}
         />
         <ul className='suggestions'>
-          
+
         </ul>
       </div>
     );
