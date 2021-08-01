26a1fbfba85a78ce2a762ddb19c6dc09e18981f7
Marie Thompson <mariethompson35@gmail.com>
2021-08-01 15:58:51 -0700

    Create tests for Number of Events

---
+++ src/_test_/NumberOfEvents.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NumberOfEvents from '../NumberOfEvents';
+
+describe('<NumberOfEvents /> component', () => {
+
+  test('render text input', () => {
+    const NumberOfEventsWrapper = shallow(<NumberOfEvents/>);
+    expect(NumberOfEventsWrapper.find('.event-count')).toHaveLength(1);
+  });
+
+  test('default number of events is 32', () => {
+    const NumberOfEventsWrapper = shallow(<NumberOfEvents/>);
+    expect(NumberOfEventsWrapper.state('eventCount')).toBe(32);
+  });
+
+  test('renders text input correctly', () => {
+    const NumberOfEventsWrapper = shallow(<NumberOfEvents/>);
+    const eventCount = NumberOfEventsWrapper.state('eventCount');
+    expect(NumberOfEventsWrapper.find('.event-count').prop('value')).toBe(eventCount);
+  });
+
+  test('change state when text input changes', () => {
+    const NumberOfEventsWrapper = shallow(<NumberOfEvents/>);
+    NumberOfEventsWrapper.setState({ eventCount: 32 });
+    const eventObject = { target: { value: 20 }};
+    NumberOfEventsWrapper.find('.event-count').simulate('change', eventObject);
+    expect(NumberOfEventsWrapper.state('eventCount')).toBe(20);
+  });
+
+});
