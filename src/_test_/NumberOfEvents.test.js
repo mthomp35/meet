ef4e4b549347828b91719df73ae3f5d946bf9369
Marie Thompson <mariethompson35@gmail.com>
2021-08-01 19:01:39 -0700

    Add wrapper to beforeAll

--- src/_test_/NumberOfEvents.test.js
+++ src/_test_/NumberOfEvents.test.js
@@ -3,30 +3,29 @@ import { shallow } from 'enzyme';
 import NumberOfEvents from '../NumberOfEvents';
 
 describe('<NumberOfEvents /> component', () => {
+  let NumberOfEventsWrapper;
+  beforeAll(() => {
+    NumberOfEventsWrapper = shallow(<NumberOfEvents/>);
+  });
 
   test('render text input', () => {
-    const NumberOfEventsWrapper = shallow(<NumberOfEvents/>);
     expect(NumberOfEventsWrapper.find('.event-count')).toHaveLength(1);
   });
 
   test('Event count is labeled', () => {
-    const NumberOfEventsWrapper = shallow(<NumberOfEvents/>);
     expect(NumberOfEventsWrapper.find('.event-count-label')).toHaveLength(1);
   });
 
   test('default number of events is 32', () => {
-    const NumberOfEventsWrapper = shallow(<NumberOfEvents/>);
     expect(NumberOfEventsWrapper.state('eventCount')).toBe(32);
   });
 
   test('renders text input correctly', () => {
-    const NumberOfEventsWrapper = shallow(<NumberOfEvents/>);
     const eventCount = NumberOfEventsWrapper.state('eventCount');
     expect(NumberOfEventsWrapper.find('.event-count').prop('value')).toBe(eventCount);
   });
 
   test('change state when text input changes', () => {
-    const NumberOfEventsWrapper = shallow(<NumberOfEvents/>);
     NumberOfEventsWrapper.setState({ eventCount: 32 });
     const eventObject = { target: { value: 20 }};
     NumberOfEventsWrapper.find('.event-count').simulate('change', eventObject);
