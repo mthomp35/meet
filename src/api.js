14ec9e7953b7986c3e9ae06ba9142eeec7af8b60
Marie Thompson <mariethompson35@gmail.com>
2022-01-12 20:14:27 -0800

    Add export getAccessToken
    Import axios to work with getAccessToken

--- src/api.js
+++ src/api.js
@@ -4,6 +4,27 @@
  * The Set will remove all duplicates from the array.
  */
 import { mockData } from './mock-data';
+import axios from 'axios';
+
+export const getAccessToken = async () => {
+  const accessToken = localStorage.getItem('access_token');
+  const tokenCheck = accessToken && (await checkToken(accessToken));
+
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    if (!code) {
+      const results = await axios.get(
+        "YOUR_SERVERLESS_GET_AUTH_URL_ENDPOINT"
+      );
+      const { authUrl } = results.data;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
+  }
+  return accessToken;
+};
 
 export const extractLocations = (events) => {
   var extractLocations = events.map((event) => event.location);
