4d1f34a6b8973e29978486e1e920be2d153b92c8
Marie Thompson <mariethompson35@gmail.com>
2022-01-16 19:44:46 -0800

    Change formatting

--- src/App.js
+++ src/App.js
@@ -55,11 +55,11 @@ class App extends Component {
           locations={this.state.locations} 
           updateEvents={this.updateEvents}
         />
-        <EventList events={this.state.events} />
         <NumberOfEvents 
           eventCount={this.state.eventCount} 
           updateEvents={this.updateEvents}
         />
+        <EventList events={this.state.events} />
       </div>
     );
   }
