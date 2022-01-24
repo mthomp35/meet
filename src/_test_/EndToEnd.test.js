2b923fb71f683dde97acbea2e10572b80e0fc4ea
Marie Thompson <mariethompson35@gmail.com>
2022-01-23 22:14:17 -0800

    Add filter events by city tests

--- src/_test_/EndToEnd.test.js
+++ src/_test_/EndToEnd.test.js
@@ -1,10 +1,14 @@
 import puppeteer from 'puppeteer';
 
 describe('show/hide an event details', () => {
+  /* https://www.ionos.com/digitalguide/hosting/technical-matters/err-connection-refused/
+  https://github.com/puppeteer/puppeteer/issues/954
+  */
   let browser;
   let page;
+  jest.setTimeout(30000);
+
   beforeAll(async () => {
-    jest.setTimeout(30000);
     browser = await puppeteer.launch({
       headless: false,
       slowMo: 250, // slow down by 250ms
@@ -35,4 +39,48 @@ describe('show/hide an event details', () => {
     const eventDetails = await page.$('.event .event_details');
     expect(eventDetails).toBeNull();
   });
+
+});
+
+describe('filter events by city', () => {
+
+  let browser;
+  let page;
+  jest.setTimeout(30000);
+
+  beforeAll(async () => {
+
+    browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 250,
+      ignoreDefaultArgs: ['--disable-extensions']
+    });
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('.CitySearch');
+    await page.waitForSelector('.event');
+  });
+
+  afterAll(() => {
+    browser.close();
+  });
+
+  test('When page is loaded, events from all cities are shown', async () => {
+    const eventsShown = await page.$('.event');
+    expect(eventsShown).toBeDefined();
+  });
+
+  test('User sees a list of suggestions when they search for a city', async () => {
+    await page.type('.city', 'Berlin');
+    const suggestions = await page.$('.suggestions');
+    expect(suggestions).toBeDefined();
+  });
+
+  test('User can select a city from the suggested list', async () => {
+    const suggestions = await page.$('.suggestions');
+    expect(suggestions).toBeDefined();
+    await page.click('.suggestions li');
+    expect('.event').toBeDefined();
+  });
+  
 });
