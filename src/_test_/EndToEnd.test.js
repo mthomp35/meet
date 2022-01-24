e35ff10f52111bd11711697a46769d4cad19cca1
Marie Thompson <mariethompson35@gmail.com>
2022-01-23 20:58:26 -0800

    Scenario 1 & 2

--- src/_test_/EndToEnd.test.js
+++ src/_test_/EndToEnd.test.js
@@ -1,4 +1,10 @@
+import puppeteer from 'puppeteer';
+
 describe('show/hide an event details', () => {
+  beforeAll(async () => {
+    jest.setTimeout(30000);
+  });
+
   test('An event element is collapsed by default', async () => {
     const browser = await puppeteer.launch();
 
@@ -11,4 +17,18 @@ describe('show/hide an event details', () => {
     expect(eventDetails).toBeNull();
     browser.close();
   });
+
+  test('User can expand an event to see its details', async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+
+    await page.waitForSelector('.event');
+    await page.click('.event .show-details');
+
+    const eventDetails = await page.$('.event .event_details');
+    expect(eventDetails).toBeDefined();
+    browser.close();
+  });
+
 });
