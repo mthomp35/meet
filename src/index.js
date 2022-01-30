3e9dc7ca7bf599662002c0a4024c0480411515ba
Marie Thompson <mariethompson35@gmail.com>
2022-01-30 11:39:01 -0800

    install atatus

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as atatus from 'atatus-spa';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
@@ -21,3 +22,5 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+atatus.config('aa406b3f0f0f44a8ac7863450dbbdf0d').install();
