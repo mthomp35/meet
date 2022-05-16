2d52f5a39a3e5b03ff6515a655643467ca9c0d67
Marie Thompson <mariethompson35@gmail.com>
2022-05-15 18:58:57 -0700

    Update username

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -27,7 +27,7 @@ function WelcomeScreen(props) {
           </button>
         </div>
       </div>
-      <a href="https://YOUR_GITHUB_USERNAME.github.io/meet/privacy.html"
+      <a href="https://mthomp35.github.io/meet/privacy.html"
       rel="nofollow noopener">
       Privacy policy
       </a>
