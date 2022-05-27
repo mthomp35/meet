d788997f69ec1e5caa3812e13bcf55c62472102a
Marie Thompson <mariethompson35@gmail.com>
2022-05-26 21:31:04 -0700

    Add responsive container

--- src/App.js
+++ src/App.js
@@ -97,19 +97,19 @@ class App extends Component {
           updateEvents={this.updateEvents}
         />
         <h4>Events in each city</h4>
-        <ScatterChart
-          width={400}
-          height={400}
-          margin={{
-            top: 20, right: 20, bottom: 20, left: 20,
-          }}
-        >
+        <ResponsiveContainer height={400}>
+          <ScatterChart
+            margin={{
+              top: 20, right: 20, bottom: 20, left: 20,
+            }}
+          >
           <CartesianGrid />
           <XAxis type='category' dataKey='city' name='city'/>
           <YAxis type='number' dataKey='number' name='number of events'/>
           <Tooltip cursor={{ strokeDasharray: '3 3' }} />
           <Scatter data={this.getData()} fill='#8884d8' />
-        </ScatterChart>
+          </ScatterChart>
+        </ResponsiveContainer>
         <EventList events={this.state.events} />
         <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen} getAccessToken={() => { getAccessToken() }} />
       </div>
