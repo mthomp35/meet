cf1d4205a14bf778360a7309909cdcde3d47422c
Marie Thompson <mariethompson35@gmail.com>
2022-05-27 21:25:08 -0700

    Add Select

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -58,12 +58,14 @@ class CitySearch extends Component {
             onFocus={() => { this.setState({ showSuggestions: true }) }}
           />
           <ul className='suggestions' style={this.state.showSuggestions ? {}: { display: 'none' }}>
-            <Select {...this.state.suggestions.map((suggestion) => (
+             {this.state.suggestions.map((suggestion) => (
               <li 
                 key={suggestion}
                 onClick={() => this.handleItemClicked(suggestion)}
-              >{suggestion}</li>
-            ))} />
+              >
+                <Select suggestion={suggestion} />
+              </li>
+            ))}
             <li 
               key='All Cities'
               onClick={() => this.handleItemClicked('All Cities')}
