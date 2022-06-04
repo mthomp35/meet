3b76af259da6793a2c2edd534c9ae74236598bf3
Marie Thompson <mariethompson35@gmail.com>
2022-06-04 16:48:36 -0700

    Removed sample data

--- src/GenrePieChart.js
+++ src/GenrePieChart.js
@@ -2,12 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
 
 const GenrePieChart = ({events}) => {
-  /*const data = [
-  { name: 'Group A', value: 400 },
-  { name: 'Group B', value: 300 },
-  { name: 'Group C', value: 300 },
-  { name: 'Group D', value: 200 },
-  ];*/
+  
   const [data, setData] = useState([]);
 
   useEffect(() => { setData(() => getData()); }, [events]);
