6d75c46c2fa6f2e1aaf944fdc5d7a6da68b33c23
Marie Thompson <mariethompson35@gmail.com>
2022-06-11 22:27:00 -0700

    Revert  getData, adj colors & sizing

--- src/GenrePieChart.js
+++ src/GenrePieChart.js
@@ -10,42 +10,23 @@ const GenrePieChart = ({events}) => {
   const getData = () => {
     const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'Angular'];
     const data = genres.map((genre) => {
-      const value = events.filter(({ summary }) => {
-        if (summary.split(" ").includes(genre)) {
-          return true;
-        }
-        if (summary.indexOf(genre) >= 0) {
-          return true;
-        }
-        return false;
-      }).length;
-      return { name: genre, value };
-    })
-    return data.filter((data) => data.value >= 1);
-  }
+      const value = events.filter(({summary}) => summary.split(' ').includes(genre)).length;
+      return { name: genre, value }
+    });
+    console.log(data);
+    return data;
+    
+  };
 
-  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+  const COLORS = ['#a8a5df', '#c5ecd9', '#bebbbb', '#f6b5ae', '#72e1fa']; //'#f3eb4f', '#9677bc' teal#00C49F
 
-/*const RADIAN = Math.PI / 180;
-
-const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
-  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-  const x = cx + radius * Math.cos(-midAngle * RADIAN);
-  const y = cy + radius * Math.sin(-midAngle * RADIAN);
-
-  return (
-    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-      {`${(percent * 100).toFixed(0)}%`}
-    </text>
-  );
-};*/
     return (
-      <ResponsiveContainer height='400'>
-        <PieChart width={400} height={400}>
+      <ResponsiveContainer height={400}>
+        <PieChart>
           <Pie
             data={data}
             labelLine={false}
-            label={({ percent }) => `${(percent * 100).toFixed(0)}%`}
+            label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
             outerRadius={80}
             fill="#8884d8"
             dataKey="value"
